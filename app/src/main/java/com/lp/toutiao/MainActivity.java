9b65692aa19fa7434de99c5a9bd16eaf5fce10ff
peng5.li <1049345904@qq.com>
2019-02-27 18:46:44 +0800

    init code

--- app/src/main/java/com/lp/toutiao/MainActivity.java
+++ app/src/main/java/com/lp/toutiao/MainActivity.java
@@ -1,20 +1,7 @@
 package com.lp.toutiao;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
-import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
